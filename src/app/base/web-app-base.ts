0dba70990b804d037e4b69e133c434d5761fd769
thanosch94 <chatzopoulosthanos@gmail.com>
2024-09-26 22:04:58 +0300

    Ver 1.0.8

--- src/app/base/web-app-base.ts
+++ src/app/base/web-app-base.ts
@@ -3,8 +3,8 @@ import { MenuItemDto } from '../dto/menu-item.dto';
 import { AppTabDto } from '../dto/app-tab.dto';
 
 export class WebAppBase {
-  static version = '1.0.7';
-  static apiVersion= '1.0.8';
+  static version = '1.0.8';
+  static apiVersion= '1.0.9';
 
   static data: any;
   static isLoggedIn: boolean;
