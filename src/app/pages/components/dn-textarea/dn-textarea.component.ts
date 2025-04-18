f7e0898438c60edb46ba6d3057ad0d1e10ceadd0
thanosch94 <chatzopoulosthanos@gmail.com>
2025-04-19 01:47:58 +0300

    Added input fields

--- src/app/pages/components/dn-textarea/dn-textarea.component.ts
+++ src/app/pages/components/dn-textarea/dn-textarea.component.ts
@@ -1,13 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, forwardRef, Input, input } from '@angular/core';
+import { FormsModule, NG_VALUE_ACCESSOR } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'dn-textarea',
-  imports: [MatFormFieldModule, MatInputModule],
+  imports: [MatFormFieldModule, MatInputModule, FormsModule],
   templateUrl: './dn-textarea.component.html',
-  styleUrl: './dn-textarea.component.css'
+  styleUrl: './dn-textarea.component.css',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => DnTextareaComponent),
+      multi: true,
+    },
+  ],
 })
 export class DnTextareaComponent {
+  label = input<string>();
+  width = input<number>(100);
+  @Input() value:string;
+  placeholder = input<string>('');
 
+
+  //#region  Reactive Forms
+  onChange: (value: string) => void = () => {
+  };
+  onTouched: () => void = () => {
+
+  };
+
+  writeValue(value: string): void {
+    this.value = value || '';
+  }
+
+  registerOnChange(fn: (value: string) => void): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+  setDisabledState?(isDisabled: boolean): void {}
+
+  //#endregion
 }
